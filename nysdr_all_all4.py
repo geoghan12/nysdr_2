55050868bcb8cf106ecc13b1fc9420dae96855fd
Geoghan <geoghan12@gmail.com>
2018-08-03 19:23:01 -0400

    friday night

--- nysdr_all_all4.py
+++ nysdr_all_all4.py
@@ -9,18 +9,23 @@ import time
 
 #<a href="javascript:document.frmMain.action.value='display_physician_info';document.frmMain.PhysicianID.value=12094889;document.frmMain.submit();" title="For more information, click here.">AHMED, SHAMIM</a>
 driver = webdriver.Chrome()
+driver.implicitly_wait(15)
 driver.get("https://www.nydoctorprofile.com/dispatch?action=process_welcome")
+
+delay=20
 
 go_to_search = driver.find_element_by_xpath("//*[@id='frmMain']/table/tbody/tr/td/table[2]/tbody/tr/td[2]/table/tbody/tr/td[2]/table[1]/tbody/tr[3]/td/table/tbody/tr[6]/td[3]/input")
 go_to_search.click()
 
-csv_file = open('allNYS2.csv', 'w')
+csv_file = open('allNYS_ped_2.csv', 'w')
 writer = csv.writer(csv_file)
 
 specialty_button= driver.find_element_by_name("SpecialtyGroupParam")
 specialty_button.click()
 specialty_opts=specialty_button.find_elements_by_tag_name("option")
-for s_ind in range(len(specialty_opts)):
+five_boros=[34,44,45,55]#[3,4,5,26,30,
+#specialty_opts=["Obstetrics and Gynecology"]
+for s_ind in [5]:#range(len(specialty_opts)):
 	specialty_button= driver.find_element_by_name("SpecialtyGroupParam")
 	specialty_button.click()
 	specialty_opts=specialty_button.find_elements_by_tag_name("option")
@@ -36,7 +41,7 @@ for s_ind in range(len(specialty_opts)):
 		counties.click()
 		county_options=counties.find_elements_by_tag_name("option")
 
-		for c_ind in range(len(county_options)):
+		for c_ind in five_boros:#range(len(county_options)):
 			#wait_counties = WebDriverWait(driver, 10)
 			# next_counties = wait_counties.until(EC.element_to_be_clickable(counties)) #(By.XPATH,'//img[@name="go11"]')
 			# next_counties.click()
@@ -48,11 +53,12 @@ for s_ind in range(len(specialty_opts)):
 			if option.text != "Any":
 				print('-'*50)
 				print(option.text)
+				county=option.text
 				option.click()
 				search_it_up=driver.find_element_by_xpath('//*[@id="frmMain"]/table/tbody/tr/td/table[2]/tbody/tr/td[2]/table/tbody/tr/td[2]/table[1]/tbody/tr[3]/td/table/tbody/tr[19]/td[1]/input')
 				search_it_up.click()
 
-				# csv_file = open('allNYS2.csv', 'w')
+				# csv_file = open('allNYS3.csv', 'w')
 				# writer = csv.writer(csv_file)
 
 				# Page index used to keep track of where we are.
@@ -81,17 +87,32 @@ for s_ind in range(len(specialty_opts)):
 							ed_link=driver.find_element_by_xpath('//img[@name="ed"]')
 							ed_link.click()
 							education=driver.find_element_by_xpath('//*[@id="frmMain"]/table[4]/tbody/tr/td/table[1]/tbody/tr[2]/td').text
+							print(education)
 
 							#practice info
 							field_link=driver.find_element_by_xpath('//img[@width="111"]')
 							field_link.click()
-							field_med=driver.find_element_by_xpath('//*[@id="frmMain"]/table[3]/tbody/tr[4]/td/table/tbody/tr/td/table[2]/tbody/tr[2]/td[1]').text
+
+							# '//*[@id="frmMain"]/table[4]'
+							# '//*[@id="frmMain"]/table[4]/tbody/tr/td/table[2]/tbody/tr/td'
+							field_xpath='//*[@id="frmMain"]/table[3]/tbody/tr[4]/td/table/tbody/tr/td/table[2]/tbody/tr[2]/td[1]'
+							# field_elem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, field_xpath)))
+							# field_med=field_elem.text
+							field_med=driver.find_element_by_xpath(field_xpath).text
 							print(field_med)
 
 							# malpractice lawsuits
+							#wait_button1 = WebDriverWait(driver, 10)
 							mal_link=driver.find_element_by_xpath('//img[@width="120"]')
+							# print('mal_link=driver.find_element_by_xpath("//img[@width="120"]"")')
 							mal_link.click()
-							malpractice=driver.find_element_by_xpath('//*[@id="frmMain"]/table[4]/tbody/tr/td/table[2]/tbody/tr/td').text
+							# print('mal_link.click()')
+							mal_xpath='//*[@id="frmMain"]/table[4]/tbody/tr/td/table[2]/tbody/tr/td'
+							mal_elem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, mal_xpath)))
+							# print('mal_elem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, mal_xpath)))')
+							
+							malpractice=mal_elem.text
+							print(malpractice)
 							if malpractice!="None reported":
 								print('malpractice!!')
 								full_mal=driver.find_elements_by_xpath('//*[@id="frmMain"]/table[4]/tbody/tr')
@@ -102,10 +123,8 @@ for s_ind in range(len(specialty_opts)):
 							phys_dict['education'] = education
 							phys_dict['field_med'] = field_med
 							phys_dict['malpractice'] = malpractice
-							phys_dict['county']=option.text
+							phys_dict['county']=county
 							writer.writerow(phys_dict.values())
-
-							#go back to results page:
 							back_button=driver.find_element_by_xpath('//img[@name="prn"]')
 							back_button.click()
 
@@ -119,7 +138,7 @@ for s_ind in range(len(specialty_opts)):
 						#break
 						print('+'*50)
 						print("new county- exception")
-						wait_button = WebDriverWait(driver, 10)
+						wait_button = WebDriverWait(driver, 3)
 						next_button = wait_button.until(EC.element_to_be_clickable((By.XPATH,
 											'//img[@name="go11"]')))
 						next_button.click()
